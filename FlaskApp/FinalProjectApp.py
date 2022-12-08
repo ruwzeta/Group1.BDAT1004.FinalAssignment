3030ff747ee61a079c1b15e2952332c012b09207
Ruwindhu Dilanga Chandraratne Hettige Don <ruwindhu123@gmail.com>
2022-12-08 13:25:08 -0500

    Unwanted code

--- FlaskApp/FinalProjectApp.py
+++ FlaskApp/FinalProjectApp.py
@@ -2,23 +2,23 @@ from flask import Flask, jsonify, request, render_template,Response
 from pymongo import MongoClient
 import pandas as pd
 import json
-from flask_charts import GoogleCharts
-from markupsafe import Markup
-from flask_charts import Chart
+# from flask_charts import GoogleCharts
+# from markupsafe import Markup
+# from flask_charts import Chart
 
 # from flask_rest_jsonapi import Api, ResourceDetail, ResourceList, ResourceRelationship
 # from flask_rest_jsonapi.exceptions import ObjectNotFound
 
 
 app = Flask(__name__)
-charts = GoogleCharts(app)
-df = pd.read_csv('../track_info4.csv')
-df.assign(Genres=df.genres.str.split(","))
-df['duration_min']= df['duration_ms'] / (1000*60)
+# charts = GoogleCharts(app)
+# df = pd.read_csv('../track_info4.csv')
+# df.assign(Genres=df.genres.str.split(","))
+# df['duration_min']= df['duration_ms'] / (1000*60)
 
-df2 = df.assign(Genres=df.genres.str.split(",")).explode('Genres')
+# df2 = df.assign(Genres=df.genres.str.split(",")).explode('Genres')
 
-data = df.to_dict(orient = "records")
+# data = df.to_dict(orient = "records")
 
 client = MongoClient("mongodb://YCH:MON2022@ac-v9dvncc-shard-00-00.6n9k6sk.mongodb.net:27017,ac-v9dvncc-shard-00-01.6n9k6sk.mongodb.net:27017,ac-v9dvncc-shard-00-02.6n9k6sk.mongodb.net:27017/?ssl=true&replicaSet=atlas-gpgpqo-shard-0&authSource=admin&retryWrites=true&w=majority")
 db = client.FlaskApp1004
